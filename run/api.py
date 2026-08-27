2a416a1a91a8983bc997c5fc68adecf0b852a232
Ruben Peters <r.peters-7@umcutrecht.nl>
2026-08-27 13:29:34 +0200

    Fix project setup

--- run/api.py
+++ run/api.py
@@ -2,7 +2,7 @@ import random
 
 import uvicorn
 
-from no_show_agent.api.app import app
+from hackathon_agentic_ai.api.app import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=random.randint(8000, 9000), log_level="info")
